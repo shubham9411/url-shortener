f5c9964353394d3d9baa0f543f9eccc402e3c040
System <none>
2017-09-23 17:13:44 +0000

    ⚡️🏨 connected with mongodb

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ URL Shortener Microservice
 User stories:
 * User Story: I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
 * User Story: When I visit that shortened URL, it will redirect me to my original link.
+* User Story: If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
 
 ### Example usage:
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
-var validUrl = require('valid-url');
+var validUrl = require('valid-url')
+var mongodb = require('mongodb').MongoClient
 var app = express()
+var url = process.env.url
+var serverurl = 'https://delicate-scene.glitch.me/'
 app.listen(process.env.PORT||8100)
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/public/index.html')
@@ -9,7 +12,16 @@ app.get('/new/*',function(req,res){
   var siteurl = req.params[0]
   console.log(siteurl)
   if (validUrl.isUri(siteurl)){
-    res.send({message:'passed parameter is a valid URL ;)'})
+    var obj = { 'siteurl': siteurl}
+    mongodb.connect(url,function(err,db){
+      if(err) throw err
+      var collection = db.collection('shortener')
+      collection.insert(obj,function(err){
+        var shorturl = serverurl+obj._id
+        res.send({original_url:siteurl,short_url: shorturl})
+      })
+      db.close();
+    })
   } else{
     res.send({error:'Wrong url format, make sure you have a valid protocol and real site.'})
   }
